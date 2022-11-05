7e953a506a68f7aa6b86989bc4acdf9654c7cd8f
GKGeovany <geovafrancisco3012@gmail.com>
2022-11-05 16:23:08 +0100

    Retouche Css

--- Modules/Module_accueil/viewAccueil.php
+++ Modules/Module_accueil/viewAccueil.php
@@ -26,39 +26,55 @@ class ViewAccueil extends vueGenerique
             <div class="l2"></div>
         </div>
 
-        <div class="container-first">
+        <section>
             <h1></span><span>Bienvenu sur </span><span>InfoHelp </span><span><br>La plateforme qui traite votre problème numérique à coup portant et en un temps éclair</span><span><br>Mais sur toi l’Eternel se lève, Sur toi sa gloire apparaît. » </span><br><br><br><br><span>Nos services</span></h1>
-            <div class="container-btns">
-              <div>
-                <div class="case c1" >
-                  <div><img src="images/conseil.webp" alt="" width="19%"></div><br>
-                  <button class="btn-first b1"><a href="#">Conseil</a></button>
-                </div>
-                <div class="case c2">
-                  <div><img src="images/reparation.webp" alt="" width="19%"></div>
-                  <button class="btn-first b2"><a href="#">Réparation</a></button>
-                </div>
+            <div id="d1">
+              <div class="card" style="width: 25rem;" id="c1">
+                <img src="images/conseil.webp" alt="" >
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Conseils</a>
+                  </div>
               </div>
-              <div>
-                <div class="case c3">
-                  <div><img src="images/vente.webp" alt="" width="19%"></div>
-                  <button class="btn-first b3"><a href="#">Vente</a></button>
-                </div>
-                <div class="case c4">
-                  <div><img src="images/tuto.webp" alt="" width="19%"></div>
-                  <button class="btn-first b4"><a href="#">Tutos</a></button>
+              <div class="card" style="width: 25rem;" id="c2">
+                <img src="images/reparation.webp" alt="">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">Reparations</a>
                 </div>
               </div>
             </div>
-        </div>
+            <div >
+              <div class="card" style="width: 25rem;" id="c3">
+                <img src="images/vente.webp" alt="">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Ventes</a>
+                  </div>
+              </div>
+              <div class="card" style="width: 25rem;" id="c4">
+                <img src="images/tuto.webp" alt="">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">Tutoriels</a>
+                </div>
+              </div>
+            </div>
+        </section>
 
 
+      <aside>
         <ul class="medias">
             <li class="bulle"><a href="#"><img src="images/facebook.png" alt="Facebook" class="logo-medias"></a></li>
             <li class="bulle"><a href="#"><img src="images/twitter.png" alt="Twitter"  class="logo-medias"></a></li>
             <li class="bulle"><a href="#"><img src="images/instagram.png" alt="Instagram" class="logo-medias"></a></li>
             <li class="bulle"><a href="#"><img src="images/youtube.png" alt="Youtube" class="logo-medias"></a></li>
         </ul>
+      </aside>
       </main>
   </body>
 </html>
