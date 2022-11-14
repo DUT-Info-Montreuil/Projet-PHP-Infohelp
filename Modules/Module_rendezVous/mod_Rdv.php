56e4cd8981f44282fb53df8eb71cff39ba2f1a85
Daniel DILIPE <dilidani2003@gmail.com>
2022-11-14 14:38:39 +0100

    verif si connecter

--- Modules/Module_rendezVous/mod_Rdv.php
+++ Modules/Module_rendezVous/mod_Rdv.php
@@ -12,41 +12,47 @@ class moduleRdv
         ConnexionUI::initConnexion();
         $this->control = new  controleurRdv;
         $this->action = isset($_GET['action']) ? $_GET['action'] : "rien";
+        if(isset($_SESSION['email'])){
+
         switch ($this->action) {
-
-            case"prendreRdv":
-                $this->control->getVue()->affichageFormRdv();
-                break; 
-
-            case "ajoutRdv":
-                $this->control->getModele()->ajouterRdv();
-                break;
-
-            case "afficherRdv":
-                $this->control->getRdv();
-                break;
-
-            case "retirerRdv":
-                $this->control->getModele()->annulerRdv();
-                break;
-
-            case "afficherListeRdv":
-                $this->control->getListeRdv();
-                break;
                 
-            case 'liste_tech':
-                $this->control->listeTechnicien();
-            
-            case 'liste_catégorie':    
-                $this->control->listeCategorie();
-                break;
+                case"prendreRdv":
+                    $this->control->getVue()->affichageFormRdv();
+                    break; 
 
-            case 'rdvTechnicien':    
-                $this->control->getModele()->modifRdv();
-                break;
+                case "ajoutRdv":
+                    $this->control->getModele()->ajouterRdv();
+                    break;
 
-            default:
-                break;
-        }
+                case "afficherRdv":
+                    $this->control->getRdv();
+                    break;
+
+                case "retirerRdv":
+                    $this->control->getModele()->annulerRdv();
+                    break;
+
+                case "afficherListeRdv":
+                    $this->control->getListeRdv();
+                    break;
+                    
+                case 'liste_tech':
+                    $this->control->listeTechnicien();
+                
+                case 'liste_catégorie':    
+                    $this->control->listeCategorie();
+                    break;
+
+                case 'rdvTechnicien':    
+                    $this->control->getModele()->modifRdv();
+                    break;
+
+                default:
+                    break;
+            }
+
+        }else {
+            echo"connectez vous";
+        }    
     }
 }
